d741b98bc947bd3aa378f610f48e35ce86f0ed39
Anderson Reyes <anderson@pymetrics.com>
2020-11-02 00:02:33 -0500

    formatting

--- main.go
+++ main.go
@@ -1,43 +1,41 @@
 package main
 
 import (
-    "io/ioutil"
 	"fmt"
-    "log"
+	"io/ioutil"
+	"log"
 	"net/http"
-    "strings"
+	"strings"
 )
 
 func HandlePingCommand(request string) string {
-    return "PONG"
+	return "PONG"
 }
-
 
 func HandleConnection(w http.ResponseWriter, r *http.Request) {
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    command := string(body)
-    var res string
+	command := string(body)
+	var res string
 
-    if strings.HasPrefix(command, "PING") {
-        res = HandlePingCommand(command)
-    } else {
-        res = "error unrecognized command"
-    }
+	if strings.HasPrefix(command, "PING") {
+		res = HandlePingCommand(command)
+	} else {
+		res = "error unrecognized command"
+	}
 
-    fmt.Fprintf(w, "%s", res)
+	fmt.Fprintf(w, "%s", res)
 }
-
 
 func main() {
 	fmt.Println("Hello, world.")
-    mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-    mux.HandleFunc("/", HandleConnection)
+	mux.HandleFunc("/", HandleConnection)
 
-    err := http.ListenAndServe(":7777", mux)
-    log.Fatal(err)
+	err := http.ListenAndServe(":7777", mux)
+	log.Fatal(err)
 }
